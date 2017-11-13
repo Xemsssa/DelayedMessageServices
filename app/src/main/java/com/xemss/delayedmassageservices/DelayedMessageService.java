f9319b443316a16b9380c4add48e23649239ccb3
Xemsssa <Xemsss@gmail.com>
2017-11-13 20:02:35 +0200

    Learning Services
    Learning how to create intent serices

---
+++ app/src/main/java/com/xemss/delayedmassageservices/DelayedMessageService.java
@@ -0,0 +1,54 @@
+package com.xemss.delayedmassageservices;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+import static android.widget.Toast.makeText;
+
+/**
+ * Created by xemss on 13.11.2017.
+ */
+
+public class DelayedMessageService extends IntentService {
+    public final static String EXTRA_MESSAGE = "message";
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+
+    public DelayedMessageService(String name) {
+        super(name);
+    }
+
+    // TODO: 13.11.2017 create constructor
+    public DelayedMessageService() {
+        super("DelayedMessageService");
+    }
+
+    // TODO: 13.11.2017 implement intent handler
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        // TODO: 13.11.2017 synchronize 
+        synchronized (this) {
+            try {
+                // TODO: 13.11.2017 wait 
+                wait(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                makeText(this, "Error start timer", Toast.LENGTH_SHORT).show();
+            }
+
+            String text = "default";
+            showText(text);
+        }
+    }
+
+    private void showText(final String text) {
+        Log.v("DelayedMessageService", "The message " + text);
+    }
+}
